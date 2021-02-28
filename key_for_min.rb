c88461a4868a25753feaf764b1bced3af2963c1b
maryamzain123 <smaryam.zain@gmail.com>
2021-02-28 07:42:47 -0600

    commit tree 153c825ca5355fa1b66414ece4f5a4d4ab45a607

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,16 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+require 'pry'
 def key_for_min_value(name_hash)
-
-end
+  
+    new_array = []
+    new_array = name_hash.collect {|name, age|
+      age}.sort
+    name_hash.each do |name, age|
+       
+      if age == new_array[0]
+        return name
+      end
+    end
+    nil 
+  end
